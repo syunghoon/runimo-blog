2c9455f7922e3047f5d672dec7476269efd1b319
syunghoon <shcho2109@gmail.com>
2025-05-14 15:04:48 +0900

    0514

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -6,11 +6,8 @@ type Props = {
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+    <div className="mx-auto max-w-2xl">
+      <div className={markdownStyles["markdown"]} dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
 }
